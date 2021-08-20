e80125229d1ce6172174a0559bf403c08afedb59
Dmitry <dikck.dmitry@yandex.ru>
2021-08-20 14:02:18 +0400

    Добавили интерактив внутри модального окна

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
 $(document).ready(function() {
     let currentFloor = 2; // переменная, где храниться текущий этаж
-    let floorPath = $('.home-image path'); // каждый отдельный этаж в SVG
-    let counterUp = $('.counter-up'); /* кнопка увеличения этажа */
-    let counterDown = $('.counter-down'); /* кнопка уменьшения этажа */
+    const floorPath = $('.home-image path'); // каждый отдельный этаж в SVG
+    const counterUp = $('.counter-up'); /* кнопка увеличения этажа */
+    const counterDown = $('.counter-down'); /* кнопка уменьшения этажа */
+    const modal = $('.modal'); // переменная в которой находиться модальное окно
+    const modalCloseButton = $('.modal-close-button'); // переменная, в которой находиться кнопка закрытия модального окна
+    const viewFlatsButton = $('.view-flats'); // кнопка, при клике на которую будет показываться модальное окно
+
+    const flatLink = $('.flat-link');
+    const flatPath = $('.flats path');
+    let currentFlat = 40;
+
+    flatLink.on('mouseover', function() {
+        flatPath.removeClass('current-flat');
+        flatLink.removeClass('active');
+        currentFlat = $(this).attr('data-flat-link');
+        $(`[data-flat=${currentFlat}]`).toggleClass('current-flat');
+    });
+
+    flatPath.on('mouseover', function() {
+        flatPath.removeClass('current-flat');
+        flatLink.removeClass('active');
+        currentFlat = $(this).attr('data-flat');
+        $(`[data-flat-link=${currentFlat}]`).toggleClass('active');
+    });
 
     // функция при наведении мышью на этаж
     floorPath.on('mouseover', function() {
@@ -10,6 +31,10 @@ $(document).ready(function() {
         currentFloor = $(this).attr('data-floor'); // получаем значение текущего этажа
         $('.counter').text(currentFloor); // записываем значение этажа в счетчик справа
     });
+
+    floorPath.on('click', toggleModal); /* при клике на этаж, вызвать модальное окно */
+    modalCloseButton.on('click', toggleModal); /* при клике на кнопку закрыть убирает окно */
+    viewFlatsButton.on('click', toggleModal); /* при клике на кнопку смотреть квартиры на этаже вызвать модальное окно */
 
     // отслеживаем клик по кнопке вверх
     counterUp.on('click', function() {
@@ -34,4 +59,9 @@ $(document).ready(function() {
             $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor'); // подсвечиваем текущий этаж
         }
     });
+
+    function toggleModal() {
+        // функция открыть-закрыть окно
+        modal.toggleClass('is-open');
+    }
 });
